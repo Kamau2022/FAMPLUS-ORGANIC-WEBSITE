9c7c1c074edcd97d363a3e38d526e16fbd41571f
kamau2022 <benkabenson@gmail.com>
2024-04-30 17:44:08 +0300

    added an item.jsx file

---
+++ frontend/src/components/Assets/data.js
@@ -0,0 +1,36 @@
+import p1_img from './avocado.png';
+import p2_img from './capsicum.png';
+import p3_img from './carrot.png';
+import p4_img from './pumpkin.png';
+
+let data_product = [
+    {
+        id: 1,
+        name: "Avocado",
+        image: p1_img,
+        new_price: 60,
+        old_price: 90,
+    }, 
+    {
+        id: 2,
+        name: "Capsicum",
+        image: p2_img,
+        new_price: 40,
+        old_price: 70,
+    }, 
+    {
+        id: 3,
+        name: "Carrot",
+        image: p3_img,
+        new_price: 30,
+        old_price: 50,
+    }, 
+    {
+        id: 4,
+        name: "Pumpkin",
+        image: p4_img,
+        new_price: 25,
+        old_price: 40,
+    }, 
+];
+export default data_product;
--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import './Item.css'
-function Item() {
+import React from 'react';
+import './Item.css';
+
+function Item(props) {
     return (
-        <div>className="item"</div>
-    )
+        <div className="item">
+            <img src={props.image} alt=""/>
+            <p>{props.name}</p>
+            <div className="item-prices">
+                <div className="item-price-new">
+                    {props.new_price}
+                </div>
+                <div className="item-price-old">
+                    {props.old_price}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Item
+export default Item;
